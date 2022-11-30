fa37912463f37c35110ff51d3a4619920218d57f
Minty <kinostl@gmail.com>
2022-11-30 04:46:01 -0500

    Fixed issue with sorting and copying.

--- src/lib/getMovesSortedByName.js
+++ src/lib/getMovesSortedByName.js
@@ -21,19 +21,37 @@ const getMovesSortedByName = async res => {
   ])
   const sortedMoveRes = await Promise.all(sortedMovePromises)
   const getEnglish = o => o.language.name === 'en'
-  const sortedMovesByName = sortedMoveRes.map(method => [
-    method[0],
-    method[1].names.filter(getEnglish)[0].name,
-    method[1].descriptions.filter(getEnglish)[0].description,
-    method[2]
-      .map(o => ({
-        ...o,
-        name: o.names.filter(getEnglish)[0].name,
-        effect: o.flavor_text_entries.filter(getEnglish).reverse()[0]
-          .flavor_text
-      }))
-      .sort((a, b) => a.name.toUpperCase().localeCompare(b.name.toUpperCase()))
-  ])
+  const moveOrder = ['level-up', 'machine', 'egg']
+  const sortedMovesByName = sortedMoveRes
+    .map(method => [
+      method[0],
+      method[1].names.filter(getEnglish)[0].name,
+      method[1].descriptions.filter(getEnglish)[0].description,
+      method[2]
+        .map(o => ({
+          ...o,
+          name: o.names.filter(getEnglish)[0].name,
+          effect: o.flavor_text_entries.filter(getEnglish).reverse()[0]
+            .flavor_text
+        }))
+        .sort((a, b) =>
+          a.name.toUpperCase().localeCompare(b.name.toUpperCase())
+        )
+    ])
+    .sort((a, b) => {
+      const aRef = moveOrder.indexOf(a[0])
+      const bRef = moveOrder.indexOf(b[0])
+      if (aRef > -1 && bRef > -1) {
+        return aRef - bRef
+      }
+      if (aRef > -1 && bRef < 0) {
+        return -1
+      }
+      if (bRef > -1 && aRef < 0) {
+        return 1
+      }
+      return 0
+    })
 
   return sortedMovesByName
 }
