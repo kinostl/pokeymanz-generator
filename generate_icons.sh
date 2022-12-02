a3f9fb6347f4439e605d71b967974dadca27dc01
Minty <kinostl@gmail.com>
2022-12-02 10:45:19 -0500

    Trimmed icons

--- generate_icons.sh
+++ generate_icons.sh
@@ -1,8 +1,8 @@
-convert -background none ./src/assets/pokeball.svg -resize 48x48 ./src/assets/favicon.ico
-convert -background none ./src/assets/pokeball.svg -resize 192x192 ./src/assets/icons/android-chrome-192x192.png 
-convert -background none ./src/assets/pokeball.svg -resize 512x512 ./src/assets/icons/android-chrome-512x512.png 
-convert -background none ./src/assets/pokeball.svg -resize 180x180 ./src/assets/icons/apple-touch-icon.png 
-convert -background none ./src/assets/pokeball.svg -resize 16x16 ./src/assets/icons/favicon-16x16.png 
-convert -background none ./src/assets/pokeball.svg -resize 32x32 ./src/assets/icons/favicon-32x32.png 
-convert -background none ./src/assets/pokeball.svg -resize 150x150 ./src/assets/icons/mstile-150x150.png 
+convert -background none -trim ./src/assets/pokeball.svg -resize 48x48 ./src/assets/favicon.ico
+convert -background none -trim ./src/assets/pokeball.svg -resize 192x192 ./src/assets/icons/android-chrome-192x192.png 
+convert -background none -trim ./src/assets/pokeball.svg -resize 512x512 ./src/assets/icons/android-chrome-512x512.png 
+convert -background none -trim ./src/assets/pokeball.svg -resize 180x180 ./src/assets/icons/apple-touch-icon.png 
+convert -background none -trim ./src/assets/pokeball.svg -resize 16x16 ./src/assets/icons/favicon-16x16.png 
+convert -background none -trim ./src/assets/pokeball.svg -resize 32x32 ./src/assets/icons/favicon-32x32.png 
+convert -background none -trim ./src/assets/pokeball.svg -resize 150x150 ./src/assets/icons/mstile-150x150.png 
 
